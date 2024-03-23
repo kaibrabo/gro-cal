73a395c54974d59233db7504335fcab43b3201a2
kainoabrabo <kubaldob@ucsc.edu>
2024-03-23 02:25:07 -0700

    update: modal design/css

--- v2/backend/controllers/plantsController.mjs
+++ v2/backend/controllers/plantsController.mjs
@@ -22,7 +22,23 @@ export function displayAddItemModal() {
 
     // add form to modal
     const form = document.createElement('form');
-    form.classList.add('add-item-form')
+    form.classList.add('add-item-form');
+
+    let nameLabel = document.createElement('label');
+    let typeLabel = document.createElement('label');
+    let startTimeLabel = document.createElement('label');
+    let vegToFlowerLabel = document.createElement('label');
+    let endTimeLabel = document.createElement('label');
+    let flowerDurationLabel = document.createElement('label');
+    let notesLabel = document.createElement('label');
+
+    nameLabel.textContent = 'Name';
+    typeLabel.textContent = 'Type';
+    startTimeLabel.textContent = 'Start Date';
+    vegToFlowerLabel.textContent = 'Flip Date (veg to flower)';
+    endTimeLabel.textContent = 'End Date';
+    flowerDurationLabel.textContent = 'Flowering Duration';
+    notesLabel.textContent = 'Notes';
     
     let nameInput = document.createElement('input');
     let typeInput = document.createElement('input');
@@ -48,13 +64,26 @@ export function displayAddItemModal() {
     endTimeInput.name = 'end-time';
     flowerDurationInput.name = 'flower-duration';
 
-    form.appendChild(nameInput)
-    form.appendChild(typeInput)
-    form.appendChild(startTimeInput)
-    form.appendChild(vegToFlowerInput)
-    form.appendChild(endTimeInput)
-    form.appendChild(flowerDurationInput)
+    form.appendChild(nameLabel);
+    form.appendChild(nameInput);
+    form.appendChild(typeLabel);
+    form.appendChild(typeInput);
+    form.appendChild(startTimeLabel);
+    form.appendChild(startTimeInput);
+    form.appendChild(vegToFlowerLabel);
+    form.appendChild(vegToFlowerInput);
+    form.appendChild(endTimeLabel);
+    form.appendChild(endTimeInput);
+    form.appendChild(flowerDurationLabel);
+    form.appendChild(flowerDurationInput);
+    form.appendChild(notesLabel);
     form.appendChild(notesInput);
+
+    // update 'end time' to reflect ('veg-to-flower' + 'flower-duration) 
+
+    // add to firebase button
+
+    // clear input
 
     modalContent.appendChild(form);
     modal.appendChild(modalContent);
